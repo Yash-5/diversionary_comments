f01c985004dd0c7a9618050ae5768dd7ebd4fd4b
Yash2208 <cs14btech11040@iith.ac.in>
2017-04-01 12:44:46 +0530

    Article extractor changed

--- newsExtractor.py
+++ newsExtractor.py
@@ -1,12 +1,4 @@
-
-# coding: utf-8
-
-# In[1]:
-
 import newspaper
-
-
-# In[2]:
 
 def extractArticle(link, getKeywords=False, getAuthors=False):
     article = newspaper.Article(url=link)
@@ -18,10 +10,3 @@ def extractArticle(link, getKeywords=False, getAuthors=False):
     if getAuthors:
         ans += [article.authors]
     return tuple(ans)
-
-
-# In[24]:
-
-link = "http://timesofindia.indiatimes.com/home/environment/pollution/after-nitish-jibe-centre-ropes-in-private-companies-to-clean-ganga/articleshow/57420309.cms"
-print extractArticle(link, getKeywords=True)
-
