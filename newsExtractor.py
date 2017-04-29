65a166df85a2bdb0445a7eb9219855f81a37ca9f
bhvjain <cs14btech11008@iith.ac.in>
2017-04-29 16:16:06 +0530

    Changed the main pipeline and removed authors and keywords from newsExtractor

--- newsExtractor.py
+++ newsExtractor.py
@@ -1,13 +1,8 @@
 import newspaper
 
-def extractArticle(link, getKeywords=False, getAuthors=False):
+def extractArticle(link):
     article = newspaper.Article(url=link)
     article.download()
     article.parse()
-    ans = [article.title, article.text]
-    if getKeywords:
-        article.nlp()
-        ans += [article.keywords]
-    if getAuthors:
-        ans += [article.authors]
-    return tuple(ans)
+    ans = article.text
+    return ans
